eac6dbe71118f496603959a6ba0e5db9f325e169
Leiv Bergmann-Behnisch <Leiv.Bergmann@elbenwald.de>
2020-01-13 13:31:27 +0100

    :sparkles: Initiual implementation of Carousel-Button

--- src/tests/CarouselButton.test.js
+++ src/tests/CarouselButton.test.js
@@ -16,8 +16,18 @@ describe('CarouselButton', () => {
   it('renders a <button>', () => {
     expect(wrapper.type()).toBe('button');
   });
-  
+
   it('passes `children` through to the <button>', () => {
     expect(wrapper.prop('children')).toBe(text);
+  });
+
+  it('passes other props through to the <button>', () => {
+    const onClick = () => {};
+    const className = 'my-carousel-button';
+    const dataAction = 'prev';
+    wrapper.setProps({ onClick, className, 'data-action': dataAction });
+    expect(wrapper.prop('onClick')).toBe(onClick);
+    expect(wrapper.prop('className')).toBe(className);
+    expect(wrapper.prop('data-action')).toBe(dataAction);
   });
 });
