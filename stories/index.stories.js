016fe2e732d79d620d1d4933330e5424ebaf699f
Leiv Bergmann-Behnisch <Leiv.Bergmann@elbenwald.de>
2020-01-27 11:32:42 +0100

    Current state.

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import Carousel from '../src/Carousel';
 import slides from '../example/slides';
 
 storiesOf('Carousel', module).add('default', () => (
-  <Carousel slides={slides} />
+  <Carousel slides={slides} onIndexChange={action('onIndexChange')} />
 ));
